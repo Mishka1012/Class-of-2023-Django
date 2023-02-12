8f043a7bde68ebe7aeefe16254376b46d250ac3f
Mishka1012 <mikhailamshei@grodnoapps.com>
2023-02-12 08:14:10 -0500

    ready for deploy

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!7ni^w)=pzrx0t4=tl%c=i(nn09hup(_#9qmjka-#h1ike=*ds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['GrodnoPythonITAcademy.pythonanywhere.com']
 
 
 # Application definition
