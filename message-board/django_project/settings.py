3f372339aa36dd5f69926ec3a213ff371635cbe2
Mishka1012 <mikhailamshei@grodnoapps.com>
2023-02-12 07:58:12 -0500

    add blog portion to the django website

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.joinpath("posts/templates/posts/")],
+        'DIRS': [BASE_DIR.joinpath("posts/templates/posts/"), BASE_DIR.joinpath("blog/templates/blog/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath("static/")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- message-board/posts/tests.py
+++ message-board/posts/tests.py
@@ -12,11 +12,11 @@ class PostsTests(TestCase):
         self.assertEqual(self.post.text, "This is a test!")
 
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
+        response = self.client.get("/posts/")
         self.assertEqual(response.status_code, 200)
 
     def test_homepage(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse("posts_list"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "This is a test!")
