7e07864ff17b9034c2b7c716ca1fed3054a17839
Mishka1012 <mikhailamshei@grodnoapps.com>
2023-02-12 08:28:46 -0500

    get static files ready for deploy

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath("static/")]
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
