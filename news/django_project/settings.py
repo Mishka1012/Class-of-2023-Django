55f09f72defd495b84f2612a7648247eae7229e1
Mishka1012 <mikhailamshei@grodnoapps.com>
2023-02-19 06:33:02 -0500

    add bootstrap crispy froms

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # crispies
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # my apps
     'accounts',
     'pages',
 ]
@@ -130,3 +134,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
