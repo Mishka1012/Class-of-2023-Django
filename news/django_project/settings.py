774a8ce87322d35a3275f9fb82a9e848c39435e1
Mishka1012 <mikhailamshei@grodnoapps.com>
2023-02-25 05:36:37 -0500

    finish work with password reset

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -139,6 +139,11 @@ LOGOUT_REDIRECT_URL = "home"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.mikhailamshei.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'python@mikhailamshei.com'
+EMAIL_HOST_PASSWORD = 'pa$$w0rd'
+DEFAULT_FROM_EMAIL = 'python@mikhailamshei.com'
 
 TIME_ZONE = "America/New_York"
